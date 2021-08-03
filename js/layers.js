4baae75a7c4eb425bfd447fe0c79fd9820039f5f
Voidsans <70246408+Voidsans@users.noreply.github.com>
2021-08-03 00:03:42 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("o", {
+    name: "originality", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "O", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,8 +8,8 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "original but bad ideas", // Name of prestige currency
+    baseResource: "bad ideas", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -24,5 +24,14 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true}, 
+    upgrades: {
+	    11: {
+		    name: "Burger",
+		    description:  "Bad idea gain is multiplied based on Original but bad ideas.",
+		    cost: new Decimal(1),
+		     effect() {
+                	return player.points.add(1).pow(0.25)
+            		},
+            		effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, 
 })
